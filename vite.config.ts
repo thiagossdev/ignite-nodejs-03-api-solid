8eb353c29fa4393da3ebfefe302787326eb982a3
Thiago S. Santos <ssthiago.dev@gmail.com>
2023-07-05 22:54:35 -0300

    Add coverage to `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      enabled: true,
+    },
+  },
 });
